41689a8651f8e9b964c17c55138e23b74a371da0
ameliachapman11 <ameliachapman06@gmail.com>
2026-04-02 13:18:44 +0100

    annotation for text user interface

--- View/TextUserInterface.java
+++ View/TextUserInterface.java
@@ -3,33 +3,38 @@ import java.util.Collection;
 import java.util.Scanner;
 
 public class TextUserInterface implements View {
-
-    private Scanner scanner = new Scanner(System.in);
-
+    @Override
     public String getInput(String inputPrompt) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println(inputPrompt);
         String input = scanner.nextLine();
+        scanner.close();
         return input;
     }
 
+    @Override
     public void displaySuccess(String successMessage) {
         System.out.println(successMessage);
     }
 
+    @Override
     public void displayError(String errorMessage) {
         System.out.println(errorMessage);
     }
 
+    @Override
     public void displayListofPerformances(Collection<String> listofPerformanceInfo) {
         for (String p : listofPerformanceInfo) {
             System.out.println(p);
         }
     }
 
+    @Override
     public void displaySpecificPerformance(String performanceInfo) {
         System.out.println(performanceInfo);
     }
 
+    @Override
     public void displayBookingRecord(String bookingRecord) {
         System.out.println(bookingRecord);
     }
