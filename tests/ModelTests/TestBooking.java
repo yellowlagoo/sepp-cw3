8f8bf54b6990aff6522f0b457c6fa4d80b737f56
ameliachapman11 <ameliachapman06@gmail.com>
2026-04-05 13:31:43 +0100

    booking tests done

--- tests/ModelTests/TestBooking.java
+++ tests/ModelTests/TestBooking.java
@@ -131,14 +131,105 @@ public class TestBooking {
         assertTrue(result.contains(student.getEmail()), "Expected true: booking record should contain student's email");
     }
 
-    //student phone number
-    //event name
-    //event ticketed
-    //event organizer
-    //performance venue
-    //performance performers
-    //performance ticket price
-    //performance sponsored
-    //performance sponsored amount
-    //performance status
+    @Test
+    @DisplayName("Testing correct student phone number for generateBookingRecord")
+    void testCorrectStudentPhoneNumber(){
+        //Sets the student's phone number as this would have been taken in when creating the booking
+        student.setPhoneNumber(12345);
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains("12345"), "Expected true: booking record should contain student's phone number");
+    }
+
+    @Test
+    @DisplayName("Testing correct event name for generateBookingRecord")
+    void testCorrectEventName(){
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains(event.getTitle()), "Expected true: booking record should contain event name");
+    }
+
+    @Test
+    @DisplayName("Testing if event is correctly ticketed in generateBookingRecord")
+    void testEventTicketedWhenTicketed(){
+        String result = booking.generateBookingRecord();
+        //Since there should only be bookings for ticketed events, it should always contain true
+        assertTrue(result.contains("true"), "Expected true: booking record should contain that event is ticketed");
+    }
+
+    @Test
+    @DisplayName("Testing correct organizer information for generateBookingRecord")
+    void testOrganizerEmail(){
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains(event.getOrganizerEmail()), "Expected true: booking record should contain organizer's email");
+    }
+
+    @Test
+    @DisplayName("Testing correct venue address for generateBookingRecord")
+    void testCorrectVenueAddress(){
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains(performance.getVenueAddress()), "Expected true: booking record should contain venue address");
+    }
+
+    @Test
+    @DisplayName("Testing correct performer names for generateBookingRecord")
+    void testCorrectPerformerNames(){
+        String result = booking.generateBookingRecord();
+        String performerNames = String.join(", ", performance.getPerformerNames());
+        assertTrue(result.contains(performerNames), "Expected true: booking record should contain performer names");
+    }
+    
+    @Test
+    @DisplayName("Testing correct ticket price for generateBookingRecord")
+    void testCorrectTicketPrice(){
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains(String.valueOf(performance.getTicketPrice())), "Expected true: booking record should contain ticket price");
+    }
+
+    @Test
+    @DisplayName("Testing correct sponsored status for generateBookingRecord when performance is not sponsored")
+    void testCorrectSponsoredStatusWhenNotSponsored(){
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains("false"), "Expected true: booking record should contain that performance is not sponsored");
+    }
+
+    @Test
+    @DisplayName("Testing correct sponsored status for generateBookingRecord when performance is sponsored")
+    void testCorrectSponsoredStatusWhenSponsored(){
+        //Sets the performance as sponsored as this would be initialized to false
+        performance.sponsor(1);
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains("true"), "Expected true: booking record should contain that performance is sponsored");
+    }
+
+    @Test
+    @DisplayName("Testing correct sponsorship amount for generateBookingRecord when performance is not sponsored")
+    void testCorrectSponsorshipAmountWhenNotSponsored(){
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains("0.0"), "Expected true: booking record should contain that sponsorship amount is 0.0");
+    }
+
+    @Test
+    @DisplayName("Testing correct sponsorship amount for generateBookingRecord when performance is sponsored")
+    void testCorrectSponsorshipAmountWhenSponsored(){
+        //Sets the performance as sponsored as this would be initialized to false
+        performance.sponsor(1);
+        String sponsorshipAmt = String.valueOf(performance.getSponsoredAmount());
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains(sponsorshipAmt), "Expected true: booking record should contain the correct sponsorship amount");
+    }
+
+    @Test
+    @DisplayName("Testing correct performance status for generateBookingRecord when performance is active")
+    void testCorrectPerformanceStatusWhenActive(){
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains("ACTIVE"), "Expected true: booking record should contain that performance is not cancelled");
+    }
+
+    @Test
+    @DisplayName("Testing correct performance status for generateBookingRecord when performance is cancelled")
+    void testCorrectPerformanceStatusWhenCancelled(){
+        //Cancels the performance as this would be initialized to active
+        performance.cancelPerformance();
+        String result = booking.generateBookingRecord();
+        assertTrue(result.contains("CANCELLED"), "Expected true: booking record should contain that performance is cancelled");
+    }
 }
