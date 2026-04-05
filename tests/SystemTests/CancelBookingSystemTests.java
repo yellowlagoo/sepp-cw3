f5dee2778a73fc4479411b13d3c6d07ca92d38f5
ameliachapman11 <ameliachapman06@gmail.com>
2026-04-05 15:23:49 +0100

    add necessary imports for system-wide tests

--- tests/SystemTests/CancelBookingSystemTests.java
+++ tests/SystemTests/CancelBookingSystemTests.java
@@ -1,3 +1,11 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 package SystemTests;
 
 public class CancelBookingSystemTests {
