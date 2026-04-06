4ebeb29a13902f26ff4fe76d6647f34981631c11
michaelduffy98980 <michaelduffy98980@gmail.com>
2026-04-06 11:25:34 +0100

    First Login test for student working for reference

--- tests/SystemTests/LogInSystemTests.java
+++ tests/SystemTests/LogInSystemTests.java
@@ -2,12 +2,45 @@ package tests.SystemTests;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.console.shadow.picocli.CommandLine.Help.Ansi.Text;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import src.View.*;
+import src.Model.*;
+import src.Controller.*;
+import src.ExternalSystems.MockVerificationSystem;
 
 public class LogInSystemTests {
-    
+
+    private TextUserInterface view;
+    private UserController userController;
+    private MockVerificationSystem mockVerificationSystem;
+
+    @BeforeEach
+    void setup() {
+
+        view = mock(TextUserInterface.class);
+        mockVerificationSystem = new MockVerificationSystem();
+        userController = new UserController(view, mockVerificationSystem);
+    }
+
+    @Test
+    @DisplayName("Testing correct value for login student email")
+    void testStudentLogin() {
+
+        when(view.getInput("Enter email:")).thenReturn("bob@hindeburgh.ed.ac.u");
+        when(view.getInput("Enter password:")).thenReturn("monkeys99$");
+
+        userController.login();
+
+        verify(view).displaySuccess("User exists: login successful");
+    }
+
 }
